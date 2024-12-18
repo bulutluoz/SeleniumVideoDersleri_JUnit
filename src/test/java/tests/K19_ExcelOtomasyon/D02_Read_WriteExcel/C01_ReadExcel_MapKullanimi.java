adc6b93e546f559905d31920f1511e633e33fdbf
bulutluoz <abbulutluoz@gmail.com>
2024-12-18 07:33:24 +0100

    Konu19 sonu

---
+++ src/test/java/tests/K19_ExcelOtomasyon/D02_Read_WriteExcel/C01_ReadExcel_MapKullanimi.java
@@ -0,0 +1,34 @@
+package tests.K19_ExcelOtomasyon.D02_Read_WriteExcel;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class C01_ReadExcel_MapKullanimi {
+
+    @Test
+    public void test01() throws IOException {
+
+        //Gerekli ayarlamalari yapip, ulkeler excelindeki Sayfa1’e gidin
+
+
+        // Eger Ingilizce ulke isimleri ve Turkce baskent isimleri ile
+        /*
+            birden fazla test varsa
+            her seferinde for loop yapmak yerine
+            data'lara daha rahat ulasabilecegimiz bir Java objesi olan Map'e kaydedebiliriz
+         */
+
+
+        // Ingilizce ismi Netherlands olan ulkenin baskentinin turkce Amsterdam oldugunu test edin
+
+
+        // Turkce baskent isimlerinde Ankara bulundugunu test edin
+
+
+
+        // Ulkeler excel'inde Ghana var mi test edin
+
+
+    }
+}
---
+++ src/test/java/tests/K19_ExcelOtomasyon/D02_Read_WriteExcel/C02_WriteExcel.java
@@ -0,0 +1,64 @@
+package tests.K19_ExcelOtomasyon.D02_Read_WriteExcel;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class C02_WriteExcel {
+
+    @Test
+    public void writeExcelTesti() throws IOException {
+
+        //3) Adimlari takip ederek Sayfa1’e kadar gidelim
+
+
+        //4) 1.satir 5.hucreye yeni bir cell olusturalim
+
+
+        //5) Olusturdugumuz hucreye “Nufus” yazdiralim
+
+
+        //6) 2.satir nufus kolonuna 1500000 yazdiralim
+
+
+        //7) 10.satir nufus kolonuna 250000 yazdiralim
+
+
+        //8) 15.satir nufus kolonuna 54000 yazdiralim
+
+
+
+        // bos olan ilk satira
+        // ingilizce ulke ismi JavaRepublic  ingilizce baskent olarak Selenium yazdiralim
+
+
+
+        //9) Dosyayi kaydedelim
+        /*
+            Biz yaptigimiz tum degisikleri
+            workbook uzerinde yaptik
+
+            workbook objesi fiziki excel dosyasinda
+            FileInputStream ile aldigimiz bilgilerle olusturuldu
+            ve excel'in bir kopyasi oldu
+
+            eger workbook uzerinde yaptigimiz degisiklikleri
+            excel dosyasina islemek istiyorsak
+            kaydetme islemi yapmamiz gerekir.
+
+            ONEMLI NOT :
+            workbook'da yaptigimiz degisiklikleri
+            excel'e kaydetme islemi yapmadan once
+            excel dosyasinin kapali oldugundan emin OLMALISINIZ.
+         */
+
+
+        //10)Dosyayi kapatalim
+
+
+    }
+
+
+}
